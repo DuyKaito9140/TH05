8a7b342ef58955d3ea7b2d1cb962019a3ff7be54
DuyKaito9140 <dcduya1nd2018@gmail.com>
2021-05-26 13:50:50 +0700

    Lab 56789 update

--- TH05/Controllers/ProductController.cs
+++ TH05/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TH05.Models;
+using PagedList;
 
 namespace TH05.Controllers
 {
@@ -11,11 +12,17 @@ namespace TH05.Controllers
     {
         DBSportStoresEntities db = new DBSportStoresEntities();
         ProductItem pro = new ProductItem();
-        public ActionResult Index() 
-        {
-            return View(pro.laydsproduct());
-        }
 
+        public ActionResult Index(int? page)
+        {
+            if (page == null) page = 1;
+            var  links = (from l in db.Products
+                         select l).OrderBy(x => x.ProductID).ToList();
+            int pageSize = 4;
+
+            int pageNumber = (page ?? 1);
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
         public ActionResult Search(double min = double.MinValue, double max = double.MaxValue)
         {
             var list = db.Products.Where(m => (double)m.Price >= min && (double)m.Price <= max).ToList();
